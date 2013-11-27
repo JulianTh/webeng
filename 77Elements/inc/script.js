81a7fe8dfa94d18041c69417da9dd78372ef6803
JulianTh <julian.theis@student.hs-rm.de>
2013-11-27 21:17:59 +0100

    Zweiter

---
+++ 77Elements/inc/script.js
@@ -0,0 +1,19 @@
+function tableHighlightRow() {
+  if (document.getElementById && document.createTextNode) {
+    var tables=document.getElementsByTagName('table');
+    for (var i=0;i<tables.length;i++)
+    {
+      if(tables[i].className=='hilite') {
+        var trs=tables[i].getElementsByTagName('tr');
+        for(var j=0;j<trs.length;j++)
+{
+          if(trs[j].parentNode.nodeName=='TBODY') {
+            trs[j].onmouseover=function(){this.className='hilight';return false}
+            trs[j].onmouseout=function(){this.className='';return false}
+          }
+        }
+      }
+    }
+  }
+}
+window.onload=function(){tableHighlightRow();} 
